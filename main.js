176494c7334508d6f8a98b1782f367f1421e2324
Josh Hayles <josh@joshhayles.com>
2023-12-07 14:48:52 -0700

    basic win/lose is working

--- main.js
+++ main.js
@@ -2,10 +2,16 @@
 const rowLength = 5;
 let currentPosition = 0; // keep track of current position
 const numberOfRows = 6;
-let maxTries = rowLength * numberOfRows;
+const maxTries = rowLength * numberOfRows;
 
 // select scoreboard-letter class
 const lettersContainer = document.querySelector(".scoreboard-letter");
+
+// create Arrays (with a length the same as numberOfRows) for storing the letters to help define each row, and each completed guess
+const rows = Array.from({ length: numberOfRows }, () => []);
+const guesses = Array.from({ length: numberOfRows }, () => []);
+
+let wordOfTheDay;
 
 // fetch word of the day
 async function fetchWordOfTheDay() {
@@ -17,7 +23,7 @@ async function fetchWordOfTheDay() {
       throw new Error(`Error fetching data: ${response.status}`);
     }
     const data = await response.json();
-    const wordOfTheDay = data.word;
+    wordOfTheDay = data.word;
     console.log(wordOfTheDay);
     return wordOfTheDay;
   } catch (error) {
@@ -27,7 +33,14 @@ async function fetchWordOfTheDay() {
 
 function handleKeyDown(event) {
   const letter = event.key;
-  //console.log(letter, currentPosition);
+
+  playTheGame(letter);
+}
+
+function playTheGame(letter) {
+  // initialize the game for a starting point
+  let guessNumber = 1;
+  let currentRow = 1;
 
   // get the current letter element based on the currentPosition
   const currentLetterElement = document.getElementById(
@@ -37,29 +50,44 @@ function handleKeyDown(event) {
   // add letter to UI
   currentLetterElement.textContent = letter;
 
+  // add letter to the current row
+  rows[currentRow - 1].push(letter);
+  console.log(rows);
+
   // increment current position
   currentPosition++;
 
-  playTheGame(letter, currentLetterElement, currentPosition);
-}
-
-function playTheGame(letter, currentLetterElement, currentPosition) {
-  console.log(`Letter: ${letter}, Letter Element: ${currentLetterElement}, Current Position: ${currentPosition}`);
+  if (currentPosition % rowLength === 0) {
+    // save their guess to the guesses Array, then increment guessNumber
+    guesses[guessNumber - 1] = rows[guessNumber - 1].slice();
+    console.log(guesses);
+    guessNumber++;
+  }
 
   if (currentPosition == maxTries) {
-    //console.log(currentPosition);
-    alert(`Sorry. You lose.`);
+    alert(`Sorry. You lose`);
     return;
   } else {
-    //
+    checkIfUserWins();
+  }
+
+  console.log(
+    `Letter: ${letter}, Position: ${currentPosition} of ${maxTries}, Guess Number: ${guessNumber} of ${numberOfRows}, Current Row: ${currentRow} of ${numberOfRows}`
+  );
+}
+
+function checkIfUserWins() {
+  // join the letters to form the user's word
+  const userWord = guesses.flat().join("");
+
+  if (userWord === wordOfTheDay) {
+    alert(`You win!!`);
+  } else {
+    return;
   }
 }
 
 async function init() {
-  // initialize the game for a starting point
-  let currentGuess = "";
-  let currentRow = 0;
-
   // fetch word of the day using await
   const wordOfTheDay = await fetchWordOfTheDay();
 
